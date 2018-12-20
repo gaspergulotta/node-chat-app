72f9bbaa38cd7ac19ead8c60bdc7449f0069a67a
gaspergulotta <gasper.gulotta@gmail.com>
2018-12-20 13:08:01 -0500

    Added geolocation feature

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,11 +1,20 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
         var message = generateMessage('Gasper', 'Welcome');
         expect(message.from).toBe('Gasper');
         expect(message.text).toBe('Welcome');
+        expect(message.createdAt).toBeA('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var message = generateLocationMessage('Gasper', 1, 2);
+        expect(message.from).toBe('Gasper');
+        expect(message.url).toBe('https://www.google.com/maps?q=1,2');
         expect(message.createdAt).toBeA('number');
     });
 });
